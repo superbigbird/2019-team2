e3d036e43fd1c634199cc0faece4331fa3f87bd5
superbigbird <2431475266@qq.com>
2019-03-06 21:07:46 +0800

    修改了UserDao.java

--- hotel/src/com/java/dao/UserDao.java
+++ hotel/src/com/java/dao/UserDao.java
@@ -10,14 +10,16 @@
 package com.java.dao;
 
 /**
- * Description:	   <br/>
- * Date:     下午12:57:15 <br/>
- * @author   丁鹏
- * @version  
- * @see 	 
+ * Description: <br/>
+ * Date: 下午12:57:15 <br/>
+ * 
+ * @author 丁鹏
+ * @version
+ * @see
  */
 public interface UserDao {
 
+    // 2019
     // 查询所有用户信息
     void getAllUsers();
 
